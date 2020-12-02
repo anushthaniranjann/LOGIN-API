69f65b713d0318b58dbf8b26f7e617601b3c6199
portfolio <anushthaniranjan@gmail.com>
2020-12-02 12:43:34 +0530

    Deleted test token

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-// res.status(503).send('Site is currently down. Check bck soon')
-// })
 
 app.use(express.json())
 app.use(userRouter)
